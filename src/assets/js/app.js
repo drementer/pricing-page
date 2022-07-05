59dcc884a01e75ebbb15f0ea81c5dc80b9857d4e
Burak <48869708+drementer@users.noreply.github.com>
2022-07-05 19:57:22 +0300

    textarea için dinamikleştirme yapıldı

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,26 @@
 // İmportlar
-import {
-	scroll_ac,
-	scroll_kapat,
-	log,
-	doc,
-	body,
-} from "./scripts/_utils.js";
+import { scroll_ac, scroll_kapat, log, doc, body } from "./scripts/_utils.js";
+
+/* Textarea */
+const ta_hizala = (element) => {
+	element.style.height = "auto";
+	element.style.height = element.scrollHeight + "px";
+};
+
+const textarealar = doc.querySelectorAll("textarea");
+
+textarealar.forEach((textarea) => {
+	// Değer girildiğinde
+	textarea.addEventListener("input", () => {
+		ta_hizala(textarea);
+
+		// İçeriği silindiğinde
+		if (element.value == undefined || element.value == "") {
+			element.style.height = "88px";
+		}
+	});
+
+	// Element yüklendiğinde
+	textarea.onload = ta_hizala(textarea);
+});
+/* Textarea SON */
